fca3cf343453aa17ba750bdd144c443754d351ed
Pratik Dubey <146483524+Pratikdubey7735@users.noreply.github.com>
2025-07-26 11:04:56 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -142,7 +142,7 @@ export default function EnhancedLoginForm() {
     setIsLoading(true);
 
     try {
-      const response = await fetch("http://localhost:5000/api/coaches/login", {
+      const response = await fetch("https://adminbackend-b9bo.onrender.com/api/coaches/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
